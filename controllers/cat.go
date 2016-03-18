8600a870d3f1469325d74e0389b61f35c6f24d6c
7x0 <neowiz@icloud.com>
2016-03-18 11:35:29 +0800

    change store model

--- controllers/cat.go
+++ controllers/cat.go
@@ -4,8 +4,7 @@ import (
     "github.com/astaxie/beego"
     "strings"
     "gopkg.in/redis.v3"
-    "fmt"
-    "strconv"
+    "math/rand"
 )
 
 const codeLength = 5
@@ -13,26 +12,12 @@ const BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 var power = [codeLength]int {14776336, 238328, 3844, 62, 1}
 var redisClient *redis.Client
 
-func IDToCode(id int) string{
+func CodeGenerator() string {
     code := ""
-    counter := codeLength - 1
-    for ; id > 0; counter-- {
-        remainder := id % 62
-        code = string(BASE62[remainder]) + code
-        id /= 62
-    }
-    for i := counter; i > -1; i-- {
-        code = "a" + code
+    for i := 0; i < codeLength; i++ {
+        code += string(BASE62[rand.Intn(61)])
     }
     return code
-}
-
-func CodeToID(code string) int {
-    id := 0
-    for i := 0; i < codeLength; i++ {
-        id += strings.Index(BASE62, string(code[i])) * power[i]
-    }
-    return id
 }
 
 func RedisConnect() {
@@ -48,37 +33,22 @@ func RedisConnect() {
         DB:       db,  // use default DB
     })
 
-    pong, err := client.Ping().Result()
-    fmt.Println(pong, err)
     redisClient = client
-    // Output: PONG <nil>
 }
 
-func RedisInit() {
-    err := redisClient.Set("id:pointer", 0, 0).Err()
+func StoreURL(tag string, target string) {
+    err := redisClient.Set("tag:"+tag, target, 0).Err()
     if err != nil {
         panic(err)
     }
 }
 
-func AssignID() int {
-    val, err := redisClient.Get("id:pointer").Result()
+func GetURL(tag string) string {
+    target, err := client.Get("tag:"+tag).Result()
     if err != nil {
         panic(err)
     }
-    id, err := strconv.Atoi(val)
-    return id + 1
-}
-
-func StoreURL(id int, target string) {
-    err := redisClient.Set("id:"+strconv.Itoa(id), target, 0).Err()
-    if err != nil {
-        panic(err)
-    } else {
-        if err := redisClient.Incr("id:pointer").Err(); err != nil {
-            panic(err)
-        }
-    }
+    return target
 }
 
 type SFO struct {    // Shovel Feces Officer
@@ -98,8 +68,7 @@ type URLWizard struct {
 }
 
 func (x *SFO) Get() {
-    RedisConnect()
-    //RedisInit()
+    //RedisConnect()
     x.Data["appName"] = beego.AppConfig.String("appname")
     x.Data["appDescription"] = beego.AppConfig.String("description")
     x.Data["appSite"] = beego.AppConfig.String("appsite")
@@ -110,9 +79,8 @@ func (s *URLShortener) Post() {
     target := s.GetString("target")
     // TODO: Target validate
     RedisConnect()
-    id := AssignID()
-    code := IDToCode(id)
-    StoreURL(id, target)
+    code := CodeGenerator()
+    StoreURL(code, target)
     s.Data["json"] = &code
     s.ServeJSON()
 }
@@ -126,5 +94,6 @@ func (c *URLChecker) Post() {
 
 
 func (w *URLWizard) Get() {
-
+    tag := w.GetString("shorten")
+    w.Redirect(GetURL(tag), 302)
 }
